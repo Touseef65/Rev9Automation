1a313d088ca33b43b8fbfea244e75366f2f32666
xiaurrehman <ziaurrehman@rev9solutions.com>
2023-05-09 15:16:43 +0500

    newFunctions added

--- src/main/java/testcases/eDocAutomation.java
+++ src/main/java/testcases/eDocAutomation.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import config.configProperties;
-
+import utils.reusebleFunctions;
 
 public class eDocAutomation {
     public static WebDriver driver=config.driverManager.Driver();
@@ -12,7 +12,7 @@ public class eDocAutomation {
     public static void Login(){
 
         driver.get(configProperties.URL);
-        driver.findElement(By.name("email")).sendKeys(configProperties.userName);
+        reusebleFunctions.enterTextInInputField("email","User01");
         driver.findElement(By.name("password")).sendKeys(configProperties.pass);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         driver.findElement(By.xpath("//a[@href='https://staging.ewaiverpro.app/edoc-admin']")).click();
